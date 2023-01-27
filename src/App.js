ce3d7be0b2475a63f853dc339f2941f0b6cc4df1
mohamad farnoud <farnoodcc@gmail.com>
2023-01-27 23:19:48 +0330

    initial project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MainLayout from "./components/layouts/MainLayout";
+import Header from "./components/ui/Header";
+
+function App() {
+  return (
+    <MainLayout>
+      <Header />
+    </MainLayout>
+  );
+}
+
+export default App;
