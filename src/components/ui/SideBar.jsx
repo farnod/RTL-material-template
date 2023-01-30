f6c8f39ce9790ec3dc100448984b199fff531dff
mohamad farnoud <farnoodcc@gmail.com>
2023-01-30 04:03:26 +0330

    footer sidebar add

--- src/components/ui/SideBar.jsx
+++ src/components/ui/SideBar.jsx
@@ -17,6 +17,8 @@ import {
 	Terminal,
 	AccountCircleRounded,
 	MessageRounded,
+	FavoriteRounded,
+	CopyrightRounded,
 } from "@mui/icons-material";
 
 function SideBar() {
@@ -30,8 +32,8 @@ function SideBar() {
 				xl={2}
 				sx={{ backgroundColor: grey[900] }}
 			>
-				<Box >
-				{/* sx={{textAlign:"center"  mt: 2, justifyContent: "center"}} */}
+				<Box>
+					{/* sx={{textAlign:"center"  mt: 2, justifyContent: "center"}} */}
 					<Hidden mdDown>
 						<Avatar
 							alt="my photo"
@@ -58,52 +60,66 @@ function SideBar() {
 						variant="scrollable"
 						ScrollButton="auto"
 						allowScrollButtonsMobile
-						
 					>
 						<Tab
 							label="صفحه اصلی"
-							icon={<HomeRounded  sx={{paddingLeft:6}}/>}
+							icon={<HomeRounded sx={{ paddingLeft: 6 }} />}
 							iconPosition="start"
-							sx={{display:"flex", justifyContent:"flex-start", }}
-							
+							sx={{ display: "flex", justifyContent: "flex-start" }}
 						/>
 						<Tab
 							label="درباره من"
-							icon={<AccountCircleRounded sx={{paddingLeft:6}} />}
+							icon={<AccountCircleRounded sx={{ paddingLeft: 6 }} />}
 							iconPosition="start"
-							sx={{display:"flex", justifyContent:"flex-start"}}
-							
+							sx={{ display: "flex", justifyContent: "flex-start" }}
 						/>
 						<Tab
 							label="رزومه من"
-							icon={<TextSnippetRounded sx={{paddingLeft:6}} />}
+							icon={<TextSnippetRounded sx={{ paddingLeft: 6 }} />}
 							iconPosition="start"
-							sx={{display:"flex", justifyContent:"flex-start"}}
-							
+							sx={{ display: "flex", justifyContent: "flex-start" }}
 						/>
-						<Tab label="نمونه کارها" 
-							icon={<Terminal sx={{paddingLeft:6}} />} 
-							iconPosition="start" 
-							sx={{display:"flex", justifyContent:"flex-start"}}
-							
-							/>
+						<Tab
+							label="نمونه کارها"
+							icon={<Terminal sx={{ paddingLeft: 6 }} />}
+							iconPosition="start"
+							sx={{ display: "flex", justifyContent: "flex-start" }}
+						/>
 						<Tab
 							label="نظرات دانشجویان"
-							icon={<MessageRounded sx={{paddingLeft:6}} />}
+							icon={<MessageRounded sx={{ paddingLeft: 6 }} />}
 							iconPosition="start"
-							sx={{display:"flex", justifyContent:"flex-start"}}
-							
+							sx={{ display: "flex", justifyContent: "flex-start" }}
 						/>
 						<Tab
 							label="ارتباط با من"
-							icon={<PhoneEnabledRounded sx={{paddingLeft:6}} />}
+							icon={<PhoneEnabledRounded sx={{ paddingLeft: 6 }} />}
 							iconPosition="start"
-							sx={{display:"flex", justifyContent:"flex-start"}}
-							
+							sx={{ display: "flex", justifyContent: "flex-start" }}
 						/>
 					</Tabs>
 					<Divider variant="middle" color={grey[600]} sx={{ mt: 2 }} />
-					<Box></Box>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							alignItems: "center",
+							height: 100,
+						}}
+					>
+						<Typography variant="subtitle2" color="whitesmoke" >
+							طراحی شده با{" "}
+							<FavoriteRounded
+								sx={{ color: "tomato", verticalAlign: "middle", height: 20 }}
+							/>
+						</Typography>
+						<Typography variant="caption" color="whitesmoke" sx={{mt:2}}>
+							کپی رایت 1401 {' '}
+							<CopyrightRounded sx={{ verticalAlign: "middle", height: 16 }} />
+						</Typography>
+					</Box>
 				</Box>
 			</Grid>
 		</>
